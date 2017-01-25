5d2f336bde0f0e512180e3bf9af1dd2799a49b1e
thepassle <pascalschilp@gmail.com>
2017-01-25 18:50:13 +0100

    updated database
    updated database with fonts + uploaded fonts

--- js/database.js
+++ js/database.js
@@ -9,6 +9,7 @@ var people = {
 		"titel" : "Het Schild: van Wapen tot Icoon",
 		"tekst" : ["empty", "empty"],
 		"egoMention" : "0",
+		"font" : "times"
 	},
 
 	"1" : {
@@ -16,6 +17,7 @@ var people = {
 		"titel" : "Parallex",
 		"tekst" : ["empty", "empty"],
 		"egoMention" : "0",
+		"font" : "times"
 	},
 
 	"2" : {
@@ -23,6 +25,7 @@ var people = {
 		"titel" : "Goed op zijn best",
 		"tekst" : ["Zo probeer ik de grenzen van moeilijk grijpbare zaken als maatschappelijke verantwoordelijkheid en beroepsethiek te verkennen door naar de praktijk te kijken.", "Maar al snel kwam ik erachter dat Sagmeister niet zomaar een ontwerper was."],
 		"egoMention" : "9",
+		"font" : "gothic"
 	},
 
 	"3" : {
@@ -30,6 +33,7 @@ var people = {
 		"titel" : "Celebrating the Anthropocene",
 		"tekst" : ["I propose that the Anthropocene is a tremendous, if not the greatest achievement of the modern Homo Sapiens.", "An example of another group that is often ridiculed for their extreme ideas, yet I praise for their dedication to set forth an actual movement, is The Voluntary Human Extinction Movement."],
 		"egoMention" : "12",
+		"font" : "times"
 	},
 
 
@@ -38,6 +42,7 @@ var people = {
 		"titel" : "Manga and anime: Reformulating the frame of mind",
 		"tekst" : ["I consider manga and anime critical design.", "I propose that fiction and work are seen as a team"],
 		"egoMention" : "13",
+		"font" : "square"
 	},
 
 
@@ -46,6 +51,7 @@ var people = {
 		"titel" : "De Digitale Lezer",
 		"tekst" : ["Ik beschouw de Electronic Literature Collection als een collectie van hulpmiddelen waarin nieuwe manieren van het vertellen van verhalen worden verzameld.", "Ik geef toe; het is handig om informatie altijd bij de hand te hebben, als iemand iets vraagt het antwoord te kunnen opzoeken, maar worden we daar aan het einde van de dag nu echt slimmer van?"],
 		"egoMention" : "14",
+		"font" : "antique"
 	},
 
 
@@ -54,6 +60,7 @@ var people = {
 		"titel" : "When USSR collapsed",
 		"tekst" : ["I began to understand that in my foreign reality, was an emergence of a Post-Soviet style—if it even can be called “style”.", "I WANT TO GIVE AN UNDERSTANDING OF MY ORIGIN CULTURE."],
 		"egoMention" : "18",
+		"font" : "lato"
 	},
 
 
@@ -62,6 +69,7 @@ var people = {
 		"titel" : "Participatory Media and Memes as Pitchforks and Torches",
 		"tekst" : ["As a design student I have always had an interest in the role of the graphic designer and the development of the field.", "As much as I declare ridicule and humor a weapon, it’s a peaceful one at that."],
 		"egoMention" : "20",
+		"font" : "caslon"
 	},
 
 
@@ -70,6 +78,7 @@ var people = {
 		"titel" : "Humanizing Technology",
 		"tekst" : ["I am a free living individual ", "I am allowed to comment anonymously "],
 		"egoMention" : "22",
+		"font" : "verdana"
 	},
 
 
@@ -78,6 +87,7 @@ var people = {
 		"titel" : "WAT ZIJN DE OORZAKEN VAN HET BESTAAN VAN CAUSALE DROGREDENEN EN WAT IS DE BESTE CONTRABEWEGING?",
 		"tekst" : ["Een mogelijke oplossing die ik als ontwerper zie, is in plaats van het nemen van een bestaande drogreden, nieuwe causale drogredenen te creëren en deze te gebruiken als instrument om het fenomeen te bestrijden.", "De conclusie die ik kan trekken naar aanleiding van dit onderzoek, is dat het bestrijden van vuur met vuur een goed begin is, maar dat we op zoek moeten naar een succesvollere oplossing op grotere schaal."],
 		"egoMention" : "23",
+		"font" : "apercu"
 	},
 
 
@@ -86,6 +96,7 @@ var people = {
 		"titel" : "Een Symbiotische Relate",
 		"tekst" : ["HTML is mijn steen en beitel"],
 		"egoMention" : "25",
+		"font" : "georgia"
 	},
 
 
@@ -94,6 +105,7 @@ var people = {
 		"titel" : "De Hybride Ontwerper",
 		"tekst" : ["Uit eigen ervaring merk ik dat je met een fotografische achtergrond bewuster omgaat met beeld van een ander", "Ik kijk naar mijn eigen hybride positie en de positie van de hedendaagse hybride ontwerper"],
 		"egoMention" : "25",
+		"font" : "opensans"
 	},
 
 
@@ -102,6 +114,7 @@ var people = {
 		"titel" : "Neuroaesthetics",
 		"tekst" : ["I am convinced that the cognitive neuroscientific approach may develop in a manner that is integral with approaches in the humanities and arts", "I remember entering a room lit through and through with yellow light"],
 		"egoMention" : "29",
+		"font" : "butler"
 	},
 
 
@@ -110,6 +123,7 @@ var people = {
 		"titel" : "It is a pleasure to see you again",
 		"tekst" : ["Though I realize, sooner or later it will come back, to make my life miserable again.", "I want to discover what the role of looping images within communication can be, what makes them attractive and therefore so ubiquitous."],
 		"egoMention" : "31",
+		"font" : "kavivanar"
 	},
 
 
@@ -118,6 +132,7 @@ var people = {
 		"titel" : "Reinventing Making",
 		"tekst" : ["This I understood as a tension between the only available agency that runs design production, where hypnosis takes place, and the other, selfanalogous, holistic, relational design practice outside of corporate interests that is active, dynamic and self-critical.", "The crisis of dualism is the essence to open up the posthuman condition, that I would like to integrate and fuse into the possibility and potentiality of design."],
 		"egoMention" : "32",
+		"font" : "creeper"
 	},
 
 
@@ -126,6 +141,7 @@ var people = {
 		"titel" : "Handle With Care!",
 		"tekst" : ["When designing I believe these layers need just as much attention as the product itself.", "I notice that I put a lot of effort into including the use of these layers in the presentation of my projects with the objective of presenting my project/concept in the most fitting way possible."],
 		"egoMention" : "33",
+		"font" : "garamond"
 	},
 
 
@@ -134,6 +150,7 @@ var people = {
 		"titel" : "All Work and No Play",
 		"tekst" : ["For the most part, I could not make my thesis if I was not playing.", "I noticed that whenever I stop looking specifically for the answer to the question that I asked myself, I would discover things that I would have never found otherwise. These random encounters with ideas and new discoveries, were in fact caused by not doing what was intended."],
 		"egoMention" : "38",
+		"font" : "arialnarrow"
 	},
 
 
@@ -141,13 +158,16 @@ var people = {
 		"name" : "Lisa van den Heuvel",
 		"titel" : "Het Lijden dat ons Bindt",
 		"tekst" : ["Ik heb het over het hoertje langs de weg, dat drugsverslaafd is en voor haar gevoel geen andere uitweg ziet.", "Ik heb het over ons als mens."],
-		"egoMention" : "45",	},
+		"egoMention" : "45",	
+		"font" : "andale"
+	},
 
 	"18" : {
 		"name" : "Haewon Lim",
 		"titel" : "The Ultimate Chair",
 		"tekst" : ["I realized that furniture designers have focused on the idea of the chair for a long time. It is the ultimate object."],
 		"egoMention" : "50",
+		"font" : "times"
 	},
 
 	"19" : {
@@ -155,6 +175,7 @@ var people = {
 		"titel" : "Concreet en Langdradig",
 		"tekst" : ["Als ik kijk naar de beeldende kunst, dan merk ik dat de werken waar een persoonlijke touch in liggen mij meer raken dan grafisch werk dat vaak erg rigide wordt uitgewerkt.", "vaakenorme) onderzoek, zie ik  Natuurlijk, ik kan met een redelijk oog beoordelen of het idee en de visualisering daarvan goed in elkaar zit of niet, maar het concept en simpelweg niet terugkomen in een set van objecten of projecties, waar ik geen persoonlijke touch -of liever gezegd, emotie- in terugzie."],
 		"egoMention" : "53",
+		"font" : "arvo"
 	},
 
 	"20" : {
@@ -162,6 +183,7 @@ var people = {
 		"titel" : "A Journey to Nothingness",
 		"tekst" : ["When I consider the concept of existence, I mention this poem", "I would define it as follows: it literally means nothing", "As a graphic design student, I am torn between aesthetically beautiful design and simplicity", "I can conclude that the essence of content should be considered as a tool that enables harmony through minimized involvement."],
 		"egoMention" : "72",
+		"font" : "times"
 	},
 
 	"21" : {
@@ -169,6 +191,7 @@ var people = {
 		"titel" : "Control & Question",
 		"tekst" : ["My obsession with the internet.", "I am completely obsessed by the possibilities of the medium but at the same time skeptical about how it is used."],
 		"egoMention" : "81",
+		"font" : "kobern"
 	},
 
 	"22" : {
@@ -176,6 +199,7 @@ var people = {
 		"titel" : "Ik Wil Vliegen In Juni",
 		"tekst" : ["Ik kan hier enkel voor mijzelf spreken en ik heb hier geen wetenschappelijke verklaring voor, maar ik vind het zelf een mooie gedachte dat wij het moment van vallen proberen te rekken.", "Ik kan het mij niet voorstellen dat er mensen zijn die zonder te kijken de sprong nemen."],
 		"egoMention" : "89",
+		"font" : "charter"
 	},
 
 	"23" : {
@@ -183,6 +207,7 @@ var people = {
 		"titel" : "Unselfish Possibilities",
 		"tekst" : ["Hospitality has always been a fascination to me", "I had experienced omotenashi and I wanted to have more of it!"],
 		"egoMention" : "94",
+		"font" : "bebas"
 	},
 
 	"24" : {
@@ -190,6 +215,7 @@ var people = {
 		"titel" : "Hoe word ik een grafische superheld?",
 		"tekst" : ["als grafisch ontwerper uit het digitale tijdperk kan ik ergens wel relateren met de mannen in dit boek", "⌘Z zit in mijn vingers en zonder ook maar een seconde te twijfelen ⌘Z ik terug naar het begin en start weer opnieuw", "Het is mijn vaste ‘wapen’, en zoals in elk traditioneel superhelden verhaal moet ik het kwaad bestrijden."],
 		"egoMention" : "187",
+		"font" : "worksans"
 	},
 
 }

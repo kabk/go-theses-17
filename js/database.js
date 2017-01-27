9a389b44eb454849c30f382a0e10200ff006776f
manegame <manusnijhoff@gmail.com>
2017-01-27 21:18:14 +0100

    Added classnames

--- js/database.js
+++ js/database.js
@@ -11,16 +11,18 @@ var people = {
 		"tekst" : ["empty", "empty"],
 		"egoMention" : "0",
 		"font" : "times",
-		"link" : "https://kabk.github.io/go-theses-17-jaap-smit/"
+		"link" : "https://kabk.github.io/go-theses-17-jaap-smit/",
+		"class" : "jaap"
 	},
 
 	"1" : {
 		"name" : "Remco Blom",
-		"titel" : "Parallex",
-		"tekst" : ["empty", "empty"],
-		"egoMention" : "0",
+		"titel" : "Parallax",
+		"tekst" : ["I focus on practical knowledge, potential problems and the adaptability of typography", "empty"],
+		"egoMention" : "19",
 		"font" : "times",
-		"link" : "https://kabk.github.io/go-theses-17-Remco-Blom-Parallax/"
+		"link" : "https://kabk.github.io/go-theses-17-Remco-Blom-Parallax/",
+		"class" : "remco"
 	},
 
 	"2" : {
@@ -29,7 +31,8 @@ var people = {
 		"tekst" : ["Zo probeer ik de grenzen van moeilijk grijpbare zaken als maatschappelijke verantwoordelijkheid en beroepsethiek te verkennen door naar de praktijk te kijken.", "Maar al snel kwam ik erachter dat Sagmeister niet zomaar een ontwerper was."],
 		"egoMention" : "9",
 		"font" : "gothic",
-		"link" : "https://kabk.github.io/go-theses-17-noa-defesche/"
+		"link" : "https://kabk.github.io/go-theses-17-noa-defesche/",
+		"class" : "noa"
 	},
 
 	"3" : {
@@ -38,7 +41,8 @@ var people = {
 		"tekst" : ["I propose that the Anthropocene is a tremendous, if not the greatest achievement of the modern Homo Sapiens.", "An example of another group that is often ridiculed for their extreme ideas, yet I praise for their dedication to set forth an actual movement, is The Voluntary Human Extinction Movement."],
 		"egoMention" : "12",
 		"font" : "times",
-		"link" : "https://kabk.github.io/go-theses-17-jordy-ringeling/"
+		"link" : "https://kabk.github.io/go-theses-17-jordy-ringeling/",
+		"class" : "jordy"
 	},
 
 
@@ -48,7 +52,8 @@ var people = {
 		"tekst" : ["I consider manga and anime critical design.", "I propose that fiction and work are seen as a team"],
 		"egoMention" : "13",
 		"font" : "square",
-		"link" : "https://kabk.github.io/go-theses-17-carmel-klein/"
+		"link" : "https://kabk.github.io/go-theses-17-carmel-klein/",
+		"class" : "carmel"
 	},
 
 
@@ -58,7 +63,8 @@ var people = {
 		"tekst" : ["Ik beschouw de Electronic Literature Collection als een collectie van hulpmiddelen waarin nieuwe manieren van het vertellen van verhalen worden verzameld.", "Ik geef toe; het is handig om informatie altijd bij de hand te hebben, als iemand iets vraagt het antwoord te kunnen opzoeken, maar worden we daar aan het einde van de dag nu echt slimmer van?"],
 		"egoMention" : "14",
 		"font" : "antique",
-		"link" : "https://kabk.github.io/go-theses-17-janne-van-hooff/"
+		"link" : "https://kabk.github.io/go-theses-17-janne-van-hooff/",
+		"class" : "janne"
 	},
 
 
@@ -68,7 +74,8 @@ var people = {
 		"tekst" : ["I began to understand that in my foreign reality, was an emergence of a Post-Soviet style—if it even can be called “style”", "I WANT TO GIVE AN UNDERSTANDING OF MY ORIGIN CULTURE"],
 		"egoMention" : "18",
 		"font" : "lato",
-		"link" : "https://kabk.github.io/go-theses-17-katerina-gaidamaka-postsoviet/"
+		"link" : "https://kabk.github.io/go-theses-17-katerina-gaidamaka-postsoviet/",
+		"class" : "katerina"
 	},
 
 
@@ -78,7 +85,8 @@ var people = {
 		"tekst" : ["As a design student I have always had an interest in the role of the graphic designer and the development of the field.", "As much as I declare ridicule and humor a weapon, it’s a peaceful one at that."],
 		"egoMention" : "20",
 		"font" : "atlas",
-		"link" : "https://kabk.github.io/go-theses-17-pascal-schilp/"
+		"link" : "https://kabk.github.io/go-theses-17-pascal-schilp/",
+		"class" : "pascal"
 	},
 
 
@@ -88,7 +96,8 @@ var people = {
 		"tekst" : ["I am a free living individual ", "I am allowed to comment anonymously "],
 		"egoMention" : "22",
 		"font" : "verdana",
-		"link" : "https://kabk.github.io/go-theses-17-Alice-Fialova-Humanizing-Technology/"
+		"link" : "https://kabk.github.io/go-theses-17-Alice-Fialova-Humanizing-Technology/",
+		"class" : "alice"
 	},
 
 
@@ -98,7 +107,8 @@ var people = {
 		"tekst" : ["Een mogelijke oplossing die ik als ontwerper zie, is in plaats van het nemen van een bestaande drogreden, nieuwe causale drogredenen te creëren en deze te gebruiken als instrument om het fenomeen te bestrijden.", "De conclusie die ik kan trekken naar aanleiding van dit onderzoek, is dat het bestrijden van vuur met vuur een goed begin is, maar dat we op zoek moeten naar een succesvollere oplossing op grotere schaal."],
 		"egoMention" : "23",
 		"font" : "apercu",
-		"link" : "https://kabk.github.io/go-theses-17-yacinth-pos/"
+		"link" : "https://kabk.github.io/go-theses-17-yacinth-pos/",
+		"class" : "yacinth"
 	},
 
 
@@ -108,7 +118,8 @@ var people = {
 		"tekst" : ["HTML is mijn steen en beitel"],
 		"egoMention" : "25",
 		"font" : "georgia",
-		"link" : "https://kabk.github.io/go-theses-17-fay-asselbergs/"
+		"link" : "https://kabk.github.io/go-theses-17-fay-asselbergs/",
+		"class" : "fay"
 	},
 
 
@@ -118,7 +129,8 @@ var people = {
 		"tekst" : ["Uit eigen ervaring merk ik dat je met een fotografische achtergrond bewuster omgaat met beeld van een ander", "Ik kijk naar mijn eigen hybride positie en de positie van de hedendaagse hybride ontwerper"],
 		"egoMention" : "25",
 		"font" : "opensans",
-		"link" : "https://kabk.github.io/go-theses-17-Laura-de-Rijk/"
+		"link" : "https://kabk.github.io/go-theses-17-Laura-de-Rijk/",
+		"class" : "laura"
 	},
 
 
@@ -128,7 +140,8 @@ var people = {
 		"tekst" : ["I am convinced that the cognitive neuroscientific approach may develop in a manner that is integral with approaches in the humanities and arts", "I remember entering a room lit through and through with yellow light"],
 		"egoMention" : "29",
 		"font" : "butler",
-		"link" : "https://kabk.github.io/go-theses-17-antonia-schwaiger/"
+		"link" : "https://kabk.github.io/go-theses-17-antonia-schwaiger/",
+		"class" : "antonia"
 	},
 
 
@@ -138,7 +151,8 @@ var people = {
 		"tekst" : ["Though I realize, sooner or later it will come back, to make my life miserable again.", "I want to discover what the role of looping images within communication can be, what makes them attractive and therefore so ubiquitous."],
 		"egoMention" : "31",
 		"font" : "kavivanar",
-		"link" : "https://kabk.github.io/go-theses-17-wies-van-der-wal/"
+		"link" : "https://kabk.github.io/go-theses-17-wies-van-der-wal/",
+		"class" : "wies"
 	},
 
 
@@ -148,7 +162,8 @@ var people = {
 		"tekst" : ["This I understood as a tension between the only available agency that runs design production, where hypnosis takes place, and the other, selfanalogous, holistic, relational design practice outside of corporate interests that is active, dynamic and self-critical.", "The crisis of dualism is the essence to open up the posthuman condition, that I would like to integrate and fuse into the possibility and potentiality of design."],
 		"egoMention" : "32",
 		"font" : "creeper",
-		"link" : "https://kabk.github.io/go-theses-17-marton-kabai/"
+		"link" : "https://kabk.github.io/go-theses-17-marton-kabai/",
+		"class" : "marton"
 	},
 
 
@@ -158,7 +173,8 @@ var people = {
 		"tekst" : ["When designing I believe these layers need just as much attention as the product itself.", "I notice that I put a lot of effort into including the use of these layers in the presentation of my projects with the objective of presenting my project/concept in the most fitting way possible."],
 		"egoMention" : "33",
 		"font" : "garamond",
-		"link" : "https://kabk.github.io/go-theses-17-sean-valies/"
+		"link" : "https://kabk.github.io/go-theses-17-sean-valies/",
+		"class" : "sean"
 	},
 
 
@@ -168,7 +184,8 @@ var people = {
 		"tekst" : ["For the most part, I could not make my thesis if I was not playing.", "I noticed that whenever I stop looking specifically for the answer to the question that I asked myself, I would discover things that I would have never found otherwise. These random encounters with ideas and new discoveries, were in fact caused by not doing what was intended."],
 		"egoMention" : "38",
 		"font" : "arialnarrow",
-		"link" : "https://kabk.github.io/go-theses-17-magda-skibinska/"
+		"link" : "https://kabk.github.io/go-theses-17-magda-skibinska/",
+		"class" : "magda"
 	},
 
 
@@ -178,7 +195,8 @@ var people = {
 		"tekst" : ["Ik heb het over het hoertje langs de weg, dat drugsverslaafd is en voor haar gevoel geen andere uitweg ziet.", "Ik heb het over ons als mens."],
 		"egoMention" : "45",
 		"font" : "andale",
-		"link" : "https://kabk.github.io/go-theses-17-lisa_van_den_heuvel_het_lijden_dat_ons_bindt/"
+		"link" : "https://kabk.github.io/go-theses-17-lisa_van_den_heuvel_het_lijden_dat_ons_bindt/",
+		"class" : "lisa"
 	},
 
 	"18" : {
@@ -187,7 +205,8 @@ var people = {
 		"tekst" : ["I realized that furniture designers have focused on the idea of the chair for a long time. It is the ultimate object."],
 		"egoMention" : "50",
 		"font" : "times",
-		"link" : "https://kabk.github.io/go-theses-17-haewon-lim/"
+		"link" : "https://kabk.github.io/go-theses-17-haewon-lim/",
+		"class" : "haewon"
 	},
 
 	"19" : {
@@ -196,7 +215,8 @@ var people = {
 		"tekst" : ["Als ik kijk naar de beeldende kunst, dan merk ik dat de werken waar een persoonlijke touch in liggen mij meer raken dan grafisch werk dat vaak erg rigide wordt uitgewerkt.", "vaakenorme) onderzoek, zie ik  Natuurlijk, ik kan met een redelijk oog beoordelen of het idee en de visualisering daarvan goed in elkaar zit of niet, maar het concept en simpelweg niet terugkomen in een set van objecten of projecties, waar ik geen persoonlijke touch -of liever gezegd, emotie- in terugzie."],
 		"egoMention" : "53",
 		"font" : "arvo",
-		"link" : "https://kabk.github.io/go-theses-17-ninthe-kiemeneij-concreet-en-langdradig/"
+		"link" : "https://kabk.github.io/go-theses-17-ninthe-kiemeneij-concreet-en-langdradig/",
+		"class" : "ninthe"
 	},
 
 	"20" : {
@@ -205,7 +225,8 @@ var people = {
 		"tekst" : ["When I consider the concept of existence, I mention this poem", "I would define it as follows: it literally means nothing", "As a graphic design student, I am torn between aesthetically beautiful design and simplicity", "I can conclude that the essence of content should be considered as a tool that enables harmony through minimized involvement."],
 		"egoMention" : "72",
 		"font" : "times",
-		"link" : "https://kabk.github.io/go-theses-17-bohye-woo-thesis-title/"
+		"link" : "https://kabk.github.io/go-theses-17-bohye-woo-thesis-title/",
+		"class" : "bohye"
 	},
 
 	"21" : {
@@ -214,7 +235,8 @@ var people = {
 		"tekst" : ["My obsession with the internet.", "I am completely obsessed by the possibilities of the medium but at the same time skeptical about how it is used."],
 		"egoMention" : "81",
 		"font" : "kobern",
-		"link" : "https://kabk.github.io/go-theses-17-manus-nijhoff/"
+		"link" : "https://kabk.github.io/go-theses-17-manus-nijhoff/",
+		"class" : "manus"
 	},
 
 	"22" : {
@@ -223,7 +245,8 @@ var people = {
 		"tekst" : ["Ik kan hier enkel voor mijzelf spreken en ik heb hier geen wetenschappelijke verklaring voor, maar ik vind het zelf een mooie gedachte dat wij het moment van vallen proberen te rekken.", "Ik kan het mij niet voorstellen dat er mensen zijn die zonder te kijken de sprong nemen."],
 		"egoMention" : "89",
 		"font" : "charter",
-		"link" : "https://kabk.github.io/go-theses-17-jamie-de-rooij/"
+		"link" : "https://kabk.github.io/go-theses-17-jamie-de-rooij/",
+		"class" : "jamie"
 	},
 
 	"23" : {
@@ -232,7 +255,8 @@ var people = {
 		"tekst" : ["Hospitality has always been a fascination to me", "I had experienced omotenashi and I wanted to have more of it!"],
 		"egoMention" : "94",
 		"font" : "bebas",
-		"link" : "https://kabk.github.io/go-theses-17-amaya-hagelaar/"
+		"link" : "https://kabk.github.io/go-theses-17-amaya-hagelaar/",
+		"class" : "amaya"
 	},
 
 	"24" : {
@@ -241,7 +265,8 @@ var people = {
 		"tekst" : ["als grafisch ontwerper uit het digitale tijdperk kan ik ergens wel relateren met de mannen in dit boek", "⌘Z zit in mijn vingers en zonder ook maar een seconde te twijfelen ⌘Z ik terug naar het begin en start weer opnieuw", "Het is mijn vaste ‘wapen’, en zoals in elk traditioneel superhelden verhaal moet ik het kwaad bestrijden."],
 		"egoMention" : "187",
 		"font" : "worksans",
-		"link" : "https://kabk.github.io/go-theses-17-carlijn-moerenhout/"
+		"link" : "https://kabk.github.io/go-theses-17-carlijn-moerenhout/",
+		"class" : "carlijn"
 	},
 }
 
@@ -320,9 +345,9 @@ for( var i = 0; i < size; i++ ){
 	// <a href='"+people[i].link+"'> "+people[i].titel+" </a></div><div class='name'><a href='"+people[i].link+"'>"+people[i].name+"</a></div>
 
 	if (i === randomStart) {
-		motherDiv.innerHTML += "<div class='slide active' id='slide"+(i+1)+"' data-anchor='slide"+(i+1)+"'><div class='title'><div class='quote'><a href='"+people[i].link+"'><h1 id='quote"+(i+1)+"'>"+people[i].tekst[Math.floor(Math.random() * people[i].tekst.length )]+"</h1></a></div></div>";
+		motherDiv.innerHTML += "<div class='slide "+people[i].class+" active' id='slide"+(i+1)+"' data-anchor='slide"+(i+1)+"'><div class='title'><div class='quote'><a href='"+people[i].link+"'><h1 id='quote"+(i+1)+"'>"+people[i].tekst[Math.floor(Math.random() * people[i].tekst.length )]+"</h1></a></div></div>";
 	} else {
-		motherDiv.innerHTML += "<div class='slide' id='slide"+(i+1)+"' data-anchor='slide"+(i+1)+"'><div class='title'><div class='quote'><a href='"+people[i].link+"'><h1 id='quote"+(i+1)+"'>"+people[i].tekst[Math.floor(Math.random() * people[i].tekst.length )]+"</h1></a></div></div>";
+		motherDiv.innerHTML += "<div class='slide "+people[i].class+"' id='slide"+(i+1)+"' data-anchor='slide"+(i+1)+"'><div class='title'><div class='quote'><a href='"+people[i].link+"'><h1 id='quote"+(i+1)+"'>"+people[i].tekst[Math.floor(Math.random() * people[i].tekst.length )]+"</h1></a></div></div>";
 	}
 
 
@@ -330,7 +355,7 @@ for( var i = 0; i < size; i++ ){
 	var quoteToBeAnimated = document.getElementById('quote'+(i+1));
 
 	quoteToBeAnimated.style.fontFamily = people[i].font;
-	
+
 	// quoteToBeAnimated.style.animation += '' + classNames[arr[0]] + ' 20s infinite';
 	// quoteToBeAnimated.style.animation += ', ' + classNames[arr[1]] + ' 23s infinite';
 	// quoteToBeAnimated.style.animation += ', ' + classNames[arr[2]] + ' 26s infinite';
@@ -361,7 +386,16 @@ $(document).ready(function(){
  			addListStyles(randomStart);
 		},
 
+		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+			$('#slide' + (slideIndex + 1)).css({
+				"background-color" : "blue"
+			});
+		},
+
 		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {
+				$('#slide' + (slideIndex)).css({
+					"background-color" : "red"
+				});
 				addListStyles(nextSlideIndex);
 		}
 

518d2c8ff4497f7b70ad360b2838919dff0446b1
thepassle <pascalschilp@gmail.com>
2017-01-27 23:57:47 +0100

    cleared texts
    and formatted commas and stuff

--- js/database.js
+++ js/database.js
@@ -28,7 +28,7 @@ var people = {
 	"2" : {
 		"name" : "Noa Defesche",
 		"titel" : "Goed op zijn best",
-		"tekst" : ["Zo probeer ik de grenzen van moeilijk grijpbare zaken als maatschappelijke verantwoordelijkheid en beroepsethiek te verkennen door naar de praktijk te kijken.", "Maar al snel kwam ik erachter dat Sagmeister niet zomaar een ontwerper was."],
+		"tekst" : ["Zo probeer ik de grenzen van moeilijk grijpbare zaken als maatschappelijke verantwoordelijkheid en beroepsethiek te verkennen door naar de praktijk te kijken", "Maar al snel kwam ik erachter dat Sagmeister niet zomaar een ontwerper was"],
 		"egoMention" : "9",
 		"font" : "gothic",
 		"link" : "https://kabk.github.io/go-theses-17-noa-defesche/",
@@ -38,7 +38,7 @@ var people = {
 	"3" : {
 		"name" : "Jordy Ringeling",
 		"titel" : "Celebrating the Anthropocene",
-		"tekst" : ["I propose that the Anthropocene is a tremendous, if not the greatest achievement of the modern Homo Sapiens.", "An example of another group that is often ridiculed for their extreme ideas, yet I praise for their dedication to set forth an actual movement, is The Voluntary Human Extinction Movement."],
+		"tekst" : ["I propose that the Anthropocene is a tremendous, if not the greatest achievement of the modern Homo Sapiens"],
 		"egoMention" : "12",
 		"font" : "times",
 		"link" : "https://kabk.github.io/go-theses-17-jordy-ringeling/",
@@ -49,7 +49,7 @@ var people = {
 	"4" : {
 		"name" : "Carmel Klein",
 		"titel" : "Manga and anime: Reformulating the frame of mind",
-		"tekst" : ["I consider manga and anime critical design.", "I propose that fiction and work are seen as a team"],
+		"tekst" : ["I consider manga and anime critical design", "I propose that fiction and work are seen as a team"],
 		"egoMention" : "13",
 		"font" : "square",
 		"link" : "https://kabk.github.io/go-theses-17-carmel-klein/",
@@ -60,7 +60,7 @@ var people = {
 	"5" : {
 		"name" : "Janne van Hooff",
 		"titel" : "De Digitale Lezer",
-		"tekst" : ["Ik beschouw de Electronic Literature Collection als een collectie van hulpmiddelen waarin nieuwe manieren van het vertellen van verhalen worden verzameld.", "Ik geef toe; het is handig om informatie altijd bij de hand te hebben, als iemand iets vraagt het antwoord te kunnen opzoeken, maar worden we daar aan het einde van de dag nu echt slimmer van?"],
+		"tekst" : ["Ik beschouw de Electronic Literature Collection als een collectie van hulpmiddelen waarin nieuwe manieren van het vertellen van verhalen worden verzameld", "Ik geef toe; het is handig om informatie altijd bij de hand te hebben"],
 		"egoMention" : "14",
 		"font" : "antique",
 		"link" : "https://kabk.github.io/go-theses-17-janne-van-hooff/",
@@ -82,7 +82,7 @@ var people = {
 	"7" : {
 		"name" : "Pascal Schilp",
 		"titel" : "Participatory Media and Memes as Pitchforks and Torches",
-		"tekst" : ["As a design student I have always had an interest in the role of the graphic designer and the development of the field.", "As much as I declare ridicule and humor a weapon, it’s a peaceful one at that."],
+		"tekst" : ["As a design student I have always had an interest in the role of the graphic designer and the development of the field", "As much as I declare ridicule and humor a weapon, it’s a peaceful one at that"],
 		"egoMention" : "20",
 		"font" : "atlas",
 		"link" : "https://kabk.github.io/go-theses-17-pascal-schilp/",
@@ -104,7 +104,7 @@ var people = {
 	"9" : {
 		"name" : "Yacinth Pos",
 		"titel" : "WAT ZIJN DE OORZAKEN VAN HET BESTAAN VAN CAUSALE DROGREDENEN EN WAT IS DE BESTE CONTRABEWEGING?",
-		"tekst" : ["Een mogelijke oplossing die ik als ontwerper zie, is in plaats van het nemen van een bestaande drogreden, nieuwe causale drogredenen te creëren en deze te gebruiken als instrument om het fenomeen te bestrijden.", "De conclusie die ik kan trekken naar aanleiding van dit onderzoek, is dat het bestrijden van vuur met vuur een goed begin is, maar dat we op zoek moeten naar een succesvollere oplossing op grotere schaal."],
+		"tekst" : ["Als ontwerper ben ik constant bezig met mijn blik op de wereld", "De conclusie die ik kan trekken naar aanleiding van dit onderzoek, is dat het bestrijden van vuur met vuur een goed begin is"],
 		"egoMention" : "23",
 		"font" : "apercu",
 		"link" : "https://kabk.github.io/go-theses-17-yacinth-pos/",
@@ -148,7 +148,7 @@ var people = {
 	"13" : {
 		"name" : "Wies van der Wal",
 		"titel" : "It is a pleasure to see you again",
-		"tekst" : ["Though I realize, sooner or later it will come back, to make my life miserable again.", "I want to discover what the role of looping images within communication can be, what makes them attractive and therefore so ubiquitous."],
+		"tekst" : ["Though I realize, sooner or later it will come back, to make my life miserable again", "I want to discover what the role of looping images within communication can be, what makes them attractive and therefore so ubiquitous"],
 		"egoMention" : "31",
 		"font" : "kavivanar",
 		"link" : "https://kabk.github.io/go-theses-17-wies-van-der-wal/",
@@ -159,7 +159,7 @@ var people = {
 	"14" : {
 		"name" : "Marton Kabai",
 		"titel" : "Reinventing Making",
-		"tekst" : ["This I understood as a tension between the only available agency that runs design production, where hypnosis takes place, and the other, selfanalogous, holistic, relational design practice outside of corporate interests that is active, dynamic and self-critical.", "The crisis of dualism is the essence to open up the posthuman condition, that I would like to integrate and fuse into the possibility and potentiality of design."],
+		"tekst" : ["The crisis of dualism is the essence to open up the posthuman condition, that I would like to integrate and fuse into the possibility and potentiality of design"],
 		"egoMention" : "32",
 		"font" : "creeper",
 		"link" : "https://kabk.github.io/go-theses-17-marton-kabai/",
@@ -170,7 +170,7 @@ var people = {
 	"15" : {
 		"name" : "Sean Valies",
 		"titel" : "Handle With Care!",
-		"tekst" : ["When designing I believe these layers need just as much attention as the product itself.", "I notice that I put a lot of effort into including the use of these layers in the presentation of my projects with the objective of presenting my project/concept in the most fitting way possible."],
+		"tekst" : ["When designing I believe these layers need just as much attention as the product itself"],
 		"egoMention" : "33",
 		"font" : "garamond",
 		"link" : "https://kabk.github.io/go-theses-17-sean-valies/",
@@ -181,7 +181,7 @@ var people = {
 	"16" : {
 		"name" : "Magda Skibinska",
 		"titel" : "All Work and No Play",
-		"tekst" : ["For the most part, I could not make my thesis if I was not playing.", "I noticed that whenever I stop looking specifically for the answer to the question that I asked myself, I would discover things that I would have never found otherwise. These random encounters with ideas and new discoveries, were in fact caused by not doing what was intended."],
+		"tekst" : ["For the most part, I could not make my thesis if I was not playing", "I noticed that whenever I stop looking specifically for the answer to the question that I asked myself, I would discover things that I would have never found otherwise"],
 		"egoMention" : "38",
 		"font" : "arialnarrow",
 		"link" : "https://kabk.github.io/go-theses-17-magda-skibinska/",
@@ -192,7 +192,7 @@ var people = {
 	"17" : {
 		"name" : "Lisa van den Heuvel",
 		"titel" : "Het Lijden dat ons Bindt",
-		"tekst" : ["Ik heb het over het hoertje langs de weg, dat drugsverslaafd is en voor haar gevoel geen andere uitweg ziet.", "Ik heb het over ons als mens."],
+		"tekst" : ["Ik heb het over het hoertje langs de weg, dat drugsverslaafd is en voor haar gevoel geen andere uitweg ziet", "Ik heb het over ons als mens"],
 		"egoMention" : "45",
 		"font" : "andale",
 		"link" : "https://kabk.github.io/go-theses-17-lisa_van_den_heuvel_het_lijden_dat_ons_bindt/",
@@ -202,7 +202,7 @@ var people = {
 	"18" : {
 		"name" : "Haewon Lim",
 		"titel" : "The Ultimate Chair",
-		"tekst" : ["I realized that furniture designers have focused on the idea of the chair for a long time. It is the ultimate object."],
+		"tekst" : ["I realized that furniture designers have focused on the idea of the chair for a long time. It is the ultimate object"],
 		"egoMention" : "50",
 		"font" : "times",
 		"link" : "https://kabk.github.io/go-theses-17-haewon-lim/",
@@ -212,7 +212,7 @@ var people = {
 	"19" : {
 		"name" : "Ninthe Kiemeneij",
 		"titel" : "Concreet en Langdradig",
-		"tekst" : ["Als ik kijk naar de beeldende kunst, dan merk ik dat de werken waar een persoonlijke touch in liggen mij meer raken dan grafisch werk dat vaak erg rigide wordt uitgewerkt.", "vaakenorme) onderzoek, zie ik  Natuurlijk, ik kan met een redelijk oog beoordelen of het idee en de visualisering daarvan goed in elkaar zit of niet, maar het concept en simpelweg niet terugkomen in een set van objecten of projecties, waar ik geen persoonlijke touch -of liever gezegd, emotie- in terugzie."],
+		"tekst" : ["Ik ben me ervan bewust dat sommige termen een specifiekere uitleg vereisen", "ik kan met een redelijk oog beoordelen of het idee en de visualisering daarvan goed in elkaar zit of niet"],
 		"egoMention" : "53",
 		"font" : "arvo",
 		"link" : "https://kabk.github.io/go-theses-17-ninthe-kiemeneij-concreet-en-langdradig/",
@@ -222,7 +222,7 @@ var people = {
 	"20" : {
 		"name" : "Bohye Woo",
 		"titel" : "A Journey to Nothingness",
-		"tekst" : ["When I consider the concept of existence, I mention this poem", "I would define it as follows: it literally means nothing", "As a graphic design student, I am torn between aesthetically beautiful design and simplicity", "I can conclude that the essence of content should be considered as a tool that enables harmony through minimized involvement."],
+		"tekst" : ["When I consider the concept of existence, I mention this poem", "I would define it as follows: it literally means nothing", "As a graphic design student, I am torn between aesthetically beautiful design and simplicity", "I can conclude that the essence of content should be considered as a tool that enables harmony through minimized involvement"],
 		"egoMention" : "72",
 		"font" : "times",
 		"link" : "https://kabk.github.io/go-theses-17-bohye-woo-thesis-title/",
@@ -232,7 +232,7 @@ var people = {
 	"21" : {
 		"name" : "Manus Nijhoff",
 		"titel" : "Control & Question",
-		"tekst" : ["My obsession with the internet.", "I am completely obsessed by the possibilities of the medium but at the same time skeptical about how it is used."],
+		"tekst" : ["My obsession with the internet", "I am completely obsessed by the possibilities of the medium but at the same time skeptical about how it is used"],
 		"egoMention" : "81",
 		"font" : "kobern",
 		"link" : "https://kabk.github.io/go-theses-17-manus-nijhoff/",
@@ -242,7 +242,7 @@ var people = {
 	"22" : {
 		"name" : "Jamie de Rooij",
 		"titel" : "Ik Wil Vliegen In Juni",
-		"tekst" : ["Ik kan hier enkel voor mijzelf spreken en ik heb hier geen wetenschappelijke verklaring voor, maar ik vind het zelf een mooie gedachte dat wij het moment van vallen proberen te rekken.", "Ik kan het mij niet voorstellen dat er mensen zijn die zonder te kijken de sprong nemen."],
+		"tekst" : ["Ik kan hier enkel voor mijzelf spreken en ik heb hier geen wetenschappelijke verklaring voor, maar ik vind het zelf een mooie gedachte dat wij het moment van vallen proberen te rekken", "Ik kan het mij niet voorstellen dat er mensen zijn die zonder te kijken de sprong nemen"],
 		"egoMention" : "89",
 		"font" : "charter",
 		"link" : "https://kabk.github.io/go-theses-17-jamie-de-rooij/",
@@ -262,7 +262,7 @@ var people = {
 	"24" : {
 		"name" : "Carlijn Moerenhout",
 		"titel" : "Hoe word ik een grafische superheld?",
-		"tekst" : ["als grafisch ontwerper uit het digitale tijdperk kan ik ergens wel relateren met de mannen in dit boek", "⌘Z zit in mijn vingers en zonder ook maar een seconde te twijfelen ⌘Z ik terug naar het begin en start weer opnieuw", "Het is mijn vaste ‘wapen’, en zoals in elk traditioneel superhelden verhaal moet ik het kwaad bestrijden."],
+		"tekst" : ["als grafisch ontwerper uit het digitale tijdperk kan ik ergens wel relateren met de mannen in dit boek", "⌘Z zit in mijn vingers en zonder ook maar een seconde te twijfelen ⌘Z ik terug naar het begin en start weer opnieuw", "Het is mijn vaste ‘wapen’, en zoals in elk traditioneel superhelden verhaal moet ik het kwaad bestrijden"],
 		"egoMention" : "187",
 		"font" : "worksans",
 		"link" : "https://kabk.github.io/go-theses-17-carlijn-moerenhout/",
